79eaa648bb9985501bc7adb8f84abab69ac2b819
Kevin Yee <kevinyee1993@gmail.com>
2018-02-15 22:14:08 -0800

    Wrote initialize, add, remove, and show methods

--- W1D5/map.rb
+++ W1D5/map.rb
@@ -1,18 +1,18 @@
 class Stack
   def initialize
-
+    @stack = []
   end
 
   def add(el)
-
+    @stack << el
   end
 
   def remove
-
+    @stack.pop
   end
 
   def show
-
+    @stack
   end
 
 end
