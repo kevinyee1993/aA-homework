70595afc44d21bf393d722c52d71491259a90801
Kevin Yee <kevinyee1993@gmail.com>
2018-02-20 21:56:30 -0800

    finished rspec hw

---
+++ W2D3/spec/dessert_spec.rb
@@ -0,0 +1,72 @@
+require 'rspec'
+require 'dessert'
+
+=begin
+Instructions: implement all of the pending specs (the `it` statements without blocks)! Be sure to look over the solutions when you're done.
+=end
+
+describe Dessert do
+    let(:chef) { double("chef", name: "Kevin" ) }
+  let(:cake) {Dessert.new("cake", 1000, chef)}
+  
+  describe "#initialize" do
+    it "sets a type" do
+        expect(cake.type).to eq("cake")
+    end
+    
+    it "sets a quantity" do
+        expect(cake.quantity).to eq(1000)
+    end
+    
+    it "starts ingredients as an empty array" do
+        expect(cake.ingredients).to eq([])
+    end
+
+    it "raises an argument error when given a non-integer quantity" do
+        expect{Dessert.new("cookies", "a", chef)}.to raise_error(ArgumentError)
+    end
+  end
+
+  describe "#add_ingredient" do
+    it "adds an ingredient to the ingredients array" do
+        cake.add_ingredient("potatoes")
+        expect(cake.ingredients).to eq(["potatoes"])
+    end
+  end
+
+  describe "#mix!" do
+    it "shuffles the ingredient array" do
+        cake.add_ingredient("potatoes")
+        cake.add_ingredient("bananas")
+        cake.add_ingredient("dog food")
+        cake.mix!
+        
+        expect(cake.ingredients).to_not eq(["potatoes", "bananas", "dog food"])
+    end
+  end
+
+  describe "#eat" do
+    it "subtracts an amount from the quantity" do
+        cake.eat(100)
+        expect(cake.quantity).to eq(900)
+    end
+
+    it "raises an error if the amount is greater than the quantity" do
+        expect{cake.eat(9001)}.to raise_error("not enough left!")
+    end
+  end
+
+  describe "#serve" do
+    it "contains the titleized version of the chef's name" do
+        allow(chef).to receive(:titleize).and_return("Chef Kevin the Great Baker")
+        expect(cake.serve).to eq("Chef Kevin the Great Baker has made 1000 cakes!")
+    end
+  end
+
+  describe "#make_more" do
+    it "calls bake on the dessert's chef with the dessert passed in" do
+        expect(chef).to receive(:bake).with(cake)
+        cake.make_more
+    end
+  end
+end
