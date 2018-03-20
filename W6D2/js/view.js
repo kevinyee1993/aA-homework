a4ac7634fe9a701da07094fe900b8c15381023d0
Kevin Yee <kevinyee1993@gmail.com>
2018-03-19 21:51:58 -0700

    finished jquery

--- W6D2/js/view.js
+++ W6D2/js/view.js
@@ -23,6 +23,7 @@ View.prototype.exercise1 = function () {
   //Result: Every square should turn orange (we already have a CSS rule)
 
   //your code here!
+  $('li').addClass("orange");
 };
 
 View.prototype.exercise2 = function () {
@@ -30,6 +31,7 @@ View.prototype.exercise2 = function () {
   //Result: Every square vanishes
 
   //your code here!
+  $('.square').remove();
 };
 
 View.prototype.exercise3 = function () {
@@ -37,6 +39,8 @@ View.prototype.exercise3 = function () {
   //Result: An <h1> with the text 'i love jquery' appears under the grid.
 
   //your code here!
+  const h1 = $('<h1>').text('i love jquery');
+  $('#easel').append(h1);
 };
 
 View.prototype.exercise4 = function () {
@@ -44,6 +48,7 @@ View.prototype.exercise4 = function () {
   //Result: Your name appears in every other square.
 
   //your code here!
+  $('.square:nth-child(2n)').text('Kevin');
 };
 
 View.prototype.exercise5 = function () {
@@ -55,6 +60,10 @@ View.prototype.exercise5 = function () {
   //  'data-pos' of every square
 
   //your code here!
+  $('.square').on('click', e => {
+    const $tile = $(e.currentTarget);
+    alert($tile.attr("data-pos"));
+  });
 };
 
 View.prototype.exercise6 = function () {
@@ -65,6 +74,11 @@ View.prototype.exercise6 = function () {
   //hint: use window._randomColorString() (defined at top) to get a random color!
 
   //your code here!
+  $('.square').each( (_, el) => {
+    var $sq = $(el);
+    $sq.css("background-color", _randomColorString());
+  });
+
 };
 
 View.prototype.exercise7 = function(){
@@ -75,6 +89,10 @@ View.prototype.exercise7 = function(){
   //rainbow.
 
   //your code here!
+  $('#easel').on('mouseenter', '.square', e => {
+    const $sq = $(e.currentTarget);
+    console.log($sq.css("background-color"));
+  });
 };
 
 
